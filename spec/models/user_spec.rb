de43981ae2d4325275b397d8d7f7a0e974843b07
Zach Easley <amissmarkoloca3@gmail.com>
2011-06-01 02:54:14 -0500

    Mod to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,6 +43,13 @@ describe User do
         invalid_email_user = User.new(@attr.merge(:email => address))
         invalid_email_user.should_not be_valid
       end
+    end
+    
+    it "should reject duplicate email addresses" do
+      # Put a user with given email address into the database.
+      User.create!(@attr)
+      user_with_duplicate_email = User.new(@attr)
+      user_with_duplicate_email.should_not be_valid
     end  
     
 end
