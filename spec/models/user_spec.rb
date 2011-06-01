77261759fddf70f58bd2f661d51431702c2c6653
Zach Easley <amissmarkoloca3@gmail.com>
2011-06-01 12:52:16 -0500

    Finished First cut of the User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,12 +24,13 @@ describe User do
     
     it "should reject names that are too long" do
     
-    long_name = 'a' * 37 
+    long_name = 'a' * 46 
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
   
   it "should accept valid email addresses" do
+    
       addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
       addresses.each do |address|
         valid_email_user = User.new(@attr.merge(:email => address))
@@ -38,11 +39,28 @@ describe User do
     end
 
     it "should reject invalid email addresses" do
+      
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
       addresses.each do |address|
         invalid_email_user = User.new(@attr.merge(:email => address))
         invalid_email_user.should_not be_valid
       end
     end  
+    
+    it "should reject duplicate email adresses" do
+      
+      # Put a user with given email address into the database.
+          User.create!(@attr)
+          user_with_duplicate_email = User.new(@attr)
+          user_with_duplicate_email.should_not be_valid
+      end
+      
+    it "should reject email adresses identical up to a case" do
+      
+      upcase_email = @attr[:email].upcase
+      User.create!(@attr.merge(:email => upcase_email))
+      user_with_duplicate_email = User.new(@attr)
+      user_with_duplicate_email.should_not be_valid
+    end
     
 end
