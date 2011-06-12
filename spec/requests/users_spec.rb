5f70c471d4040b8d4f98678b73a94ca3a3a39f1a
Zach Easley <amissmarkoloca3@gmail.com>
2011-06-12 00:39:31 -0500

    Finished with the Sign-up

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe "Users" do
-<<<<<<< HEAD
-  
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_index_path
-      response.status.should be(200)
-    end
-  end
-  
+
   describe "signup" do
     
     describe "failure" do
