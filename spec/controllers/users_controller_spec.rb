1d198046f7ec882b9d96143bbeae3b04f6db6a9f
Zach Easley <amissmarkoloca3@gmail.com>
2011-06-20 10:33:07 -0500

    Fixed one error

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -214,7 +214,7 @@ end
       
       it "should deny access to edit" do
         get :edit, :id => @user
-        response.should redirect_to(user_path)
+        response.should redirect_to(signin_path)
       end
       
       it "should deny access to update" do
@@ -226,7 +226,7 @@ end
     describe "for signed in users" do
       
       before(:each) do
-        wrong_user = Factory(:user => :email => "user@example.com")
+        wrong_user = Factory(:user, :email => "user@example.net")
         test_sign_in(wrong_user)
       end
       
