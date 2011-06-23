9df4d90c96a4e61616b813bb7f32cced2cd7de1e
Zach Easley <amissmarkoloca3@gmail.com>
2011-06-23 11:04:02 -0500

    Added microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,10 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
-  end
+   end
+  
   
   def new
     @user = User.new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110622225551) do
+ActiveRecord::Schema.define(:version => 20110623153030) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "content"
+    t.integer  "message_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
