a5dd6a21c80965b38e8de633493d6dee37605c98
Zach Easley <amissmarkoloca3@gmail.com>
2011-06-22 23:22:26 -0500

    Built microposts db model

---
+++ app/models/micropost.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+# Schema version: 20110622225551
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Micropost < ActiveRecord::Base
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110604043611
+# Schema version: 20110622225551
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
